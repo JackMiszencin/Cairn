ec14a2ffdc356f93135c76c891bfe04b47898f3a
JackMiszencin <jack.miszencin@gmail.com>
2014-03-11 21:55:54 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cairn::Application.config.secret_key_base = '3fc17ecf8bfd6761b9917e342ce2c9af45477bd99ee196b34ba97f9d3e293ba24e802131184fdb15cb0ed9690f29f5954c7f1b50c0ce3175bfd2fecd04ba1bd3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cairn::Application.config.session_store :cookie_store, key: '_Cairn_session'
