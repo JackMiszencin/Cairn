16445160f98f4dd5bb2f2577cb40852b5615d982
JackMiszencin <jack.miszencin@gmail.com>
2014-09-24 22:13:08 -0400

    got most of tag form persisting

--- app/assets/javascripts/views/tags/form.js
+++ app/assets/javascripts/views/tags/form.js
@@ -1,7 +1,15 @@
-var radiusDefault = Number($("#tag_radius").val());
+var radiusDefault, latLngDefault;
 var mapScope, realmLat, realmLng, markerCircle;
 
 function setDefaults() {
+	if ($("#tag_radius").val()) {
+		radiusDefault = Number($("#tag_radius").val());
+	}
+	var latDefault = $("#hidden_tag_lat").val();
+	var lngDefault = $("#hidden_tag_lng").val();
+	if (latDefault && lngDefault) {
+		latLngDefault = new google.maps.LatLng(Number(latDefault), Number(lngDefault));
+	}
 	var radiusDegreesY = Number($("#hidden_realm_radius").val() || 55);
 	var radiusDegreesX = Number($("#hidden_realm_radius").val() || 100);
 	realmLat = Number($("#hidden_realm_lat").val() || 0);
@@ -15,7 +23,7 @@ function addCircle(map, markerPoint){
 	markerCircle = new google.maps.Circle({
 		map:map,
 		center:markerPoint,
-		radius:radiusDefault,
+		radius:(radiusDefault || 0),
 		fillColor:"#EEEEEE",
 		fillOpacity:0.5,
 		strokeColor:"#EEEEEE",
@@ -47,6 +55,10 @@ function addMarker(event, map){
 	$("#tag_center").val(lngLatString);
 	$("#submit_tag").attr("type", "submit") // TODO: Do we need this? What the fuck is this for?
 
+	// If setDefaults() has assigned us a default radius already, we might as well make it a circle.
+	if (radiusDefault) {
+		addCircle(map, markerPoint);
+	}
 	google.maps.event.addListenerOnce(map, 'click',
 		function() {
 			addCircle(map, markerPoint);
@@ -56,40 +68,62 @@ function addMarker(event, map){
 	google.maps.event.addListener(marker, 'drag', function() {
 		recenterCircle(marker, markerCircle);
 	});
-	$("#tag_radius").on("click", function(){
-		newRadius = Number($(this).val());
-		markerCircle.setRadius(newRadius);
-	});
-	$("#tag_radius").on("keyup", function(){
-		rFeed = String($(this).val());
-		newRadius = Number(rFeed);
-		markerCircle.setRadius(newRadius);
-	});
 };
 
-
-function initialize() {
-	// setDefaults
-	setDefaults();
+// Creates and returns a google map object;
+function createMap() {
 	var mapOptions = {
 	    center: new google.maps.LatLng(realmLat, realmLng),
 	    zoom: 8,
 	    mapTypeId: google.maps.MapTypeId.HYBRID
 	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-	google.maps.event.addListenerOnce(map, 'click',
-		function (e) {
-			addMarker(e, map);
+	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);	
+	return map;
+}
+
+function setRadiusListeners() {
+	function tagCallback() {
+		radiusDefault = Number($("#tag_radius").val());
+		if (markerCircle) {
+			markerCircle.setRadius(radiusDefault);
 		}
-	);
-	$("#tag_tag_type_id").on('click', function(){
+	}
+	$("#tag_radius").on('click', tagCallback);
+	$("#tag_radius").on('keyup', tagCallback);	
+}
+
+function setTagTypeListener() {
+	function tagTypeCallback() {
 		var typeId = String($("#tag_tag_type_id").val());
 		typeId = "#tag_type_" + typeId;
-		radiusDefault = Number($(typeId).val());
-		$("#tag_radius").val(radiusDefault);
-		markerCircle.setRadius(radiusDefault);
-	});
+		if ($(typeId).val()) {
+			radiusDefault = Number($(typeId).val());
+			$("#tag_radius").val(radiusDefault);
+			if (markerCircle) {
+				markerCircle.setRadius(radiusDefault);
+			}
+		}
+	}
+	$("#tag_tag_type_id").on('click', tagTypeCallback);
+	$("#tag_tag_type_id").on('keyup', tagTypeCallback);
+}
+
+function initialize() {
+	setDefaults();
+	var map = createMap();
+	setRadiusListeners();
+	setTagTypeListener();
+	google.maps.event.addListenerOnce(map, 'click', function (e) { console.log("e", e); addMarker(e, map); });
+
 	map.fitBounds(mapScope);
+
+	if (radiusDefault && latLngDefault) {
+		var obj = {
+			latLng: latLngDefault
+		}
+		addMarker(obj, map);
+	}
+
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
--- app/models/atlas.rb
+++ app/models/atlas.rb
@@ -1,5 +1,6 @@
 class Atlas < ActiveRecord::Base
 	serialize :type_filters, Array
 	belongs_to :realm, :class_name => 'Tag'
+	accepts_nested_attributes_for :realm
 
 end
