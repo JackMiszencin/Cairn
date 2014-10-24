790294f1ebbea783aa6df767aac133afc0eabd8a
JackMiszencin <jack.miszencin@gmail.com>
2014-10-23 23:39:25 -0400

    tag.relevant_tags working. Needs refactor since current sorting sql is inefficient

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,48 @@ class Tag < ActiveRecord::Base
 	before_save :set_circle
 	belongs_to :tag_typeexi
 
+	scope :intersects_geometry, -> (geo) {
+		where(%{
+			ST_Intersects(
+				tags.shape,
+				ST_GeographyFromText('SRID=4326;%s')
+			)
+		} % [geo])
+	}
+
+	scope :sort_by_relevance, -> (geo) {
+		order(%{
+			(
+				(
+					st_area(
+						ST_Intersection(
+							tags.shape,
+							ST_GeographyFromText('SRID=4326;%s')
+						)
+					) *
+					st_area(
+						ST_Intersection(
+							tags.shape,
+							ST_GeographyFromText('SRID=4326;%s')
+						)
+					)
+				) /
+				(
+					st_area(
+						ST_GeographyFromText('SRID=4326;%s')
+					) *
+					st_area(					
+						tags.shape
+					)
+				)
+			)
+		} % [geo, geo, geo])
+	}
+
+	def relevant_tags
+		Tag.intersects_geometry(self.shape).sort_by_relevance(self.shape)
+	end
+
 	def set_circle
 		ok = true
 		if self.center == nil
